6f154f93c55f1d6678621bcdc43dc683d48a9a92
Milkin1 <mammadov.ilkin@hotmail.com>
2021-09-22 09:04:54 +0400

    Секция квест комната. Экстренный пуш

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,48 @@
 "use strict"
-const body =document.querySelector("body");
-const html=document.querySelector("html");
-const header=document.querySelector(".header");
-const navBtn=document.querySelector(".nav__btn");
- 
-window.onload= function(){
-  pagescroll(); 
-}  ;
- 
-      @@include('modules/nav.js') 
-      @@include('modules/fixed-header.js')
-  
-      @@include('lazyscripts.js')
-      @@include('modules/accordion.js')
+const body = document.querySelector("body");
+const html = document.querySelector("html");
+const header = document.querySelector(".header");
+const navBtn = document.querySelector(".nav__btn");
 
- 
+window.onload = function () {
+  pagescroll();
+};
 
 
 
+
+function anchoreDropdown() {
+  document.getElementById("dropdownlist").classList.toggle("show");
+}
+
+// Закройте выпадающее меню, если пользователь щелкает за его пределами
+window.onclick = function (event) {
+  if (!event.target.matches('.dropdown__link')) {
+    var dropdowns = document.getElementsByClassName("content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+function questDropdown() {
+  document.getElementById("quest-dropdownlist").classList.toggle("show");
+}
+
+// Закройте выпадающее меню, если пользователь щелкает за его пределами
+window.onclick = function (event) {
+  if (!event.target.matches('.quest-dropdown__link')) {
+    var dropdowns = document.getElementsByClassName("quest-dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
