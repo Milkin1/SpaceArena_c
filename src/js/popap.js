a58ac90f17fb1953d6d63f981121a550b9ca697e
Milkin1 <mammadov.ilkin@hotmail.com>
2021-09-28 10:43:17 +0400

    исправил таблицу, блоки с картинками и текстами

--- src/js/popap.js
+++ src/js/popap.js
@@ -1,13 +1,29 @@
 // Модальные окна
 
 // const click = document.querySelectorAll("click");
-const body = document.getElementById('body');
-const clicks = document.querySelectorAll("click");
+// const body = document.getElementById('body');
+// const clicks = document.querySelectorAll("click");
+const closeBtns = document.querySelectorAll('[data-close]');
+const openBtns = document.querySelectorAll('[data-open]');
+const popup = document.querySelectorAll('.popup');
 
-
-
-clicks.forEach(click => {
-    click.addEventListener('click', event => {
-        body.classList.add("lock")
+console.log(popup);
+for (let closeBtn of closeBtns) {
+    closeBtn.addEventListener('click', function () {
+        popup.classList.remove("open");
     })
-})
+}
+
+for (let openBtn of openBtns) {
+    openBtn.addEventListener('click', function (e) {
+        popup.classList.add("open");
+    })
+}
+
+
+
+// clicks.forEach(click => {
+//     click.addEventListener('click', event => {
+//         body.classList.add("lock")
+//     })
+// })
