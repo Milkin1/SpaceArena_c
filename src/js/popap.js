396e5f363f867d61811fd57975a261a57f45d22b
Milkin1 <mammadov.ilkin@hotmail.com>
2021-09-29 06:21:11 +0400

    попап сделал. ошибки исправил

--- src/js/popap.js
+++ src/js/popap.js
@@ -1,29 +1,41 @@
 // Модальные окна
 
-// const click = document.querySelectorAll("click");
-// const body = document.getElementById('body');
-// const clicks = document.querySelectorAll("click");
-const closeBtns = document.querySelectorAll('[data-close]');
-const openBtns = document.querySelectorAll('[data-open]');
-const popup = document.querySelectorAll('.popup');
+console.clear();
+const popupText = document.querySelector('.popup-text');
+const popupImage = document.querySelector('.popup-image');
 
-console.log(popup);
-for (let closeBtn of closeBtns) {
-    closeBtn.addEventListener('click', function () {
-        popup.classList.remove("open");
-    })
-}
+const popupTextBtn = document.querySelector('[data-btn-text]');
+const popupImageBtn = document.querySelector('[data-btn-image]');
 
-for (let openBtn of openBtns) {
-    openBtn.addEventListener('click', function (e) {
-        popup.classList.add("open");
-    })
-}
+const popupTextCloseBtn = document.querySelector('[data-popup-text]')
+const popupImageCloseBtn = document.querySelector('[data-popup-image]')
 
+const popupTextCloseBtnX = document.querySelector('[data-popup-text-x]')
+const popupImageCloseBtnX = document.querySelector('[data-popup-image-x]')
 
+const lockBobyScroll = document.body
 
-// clicks.forEach(click => {
-//     click.addEventListener('click', event => {
-//         body.classList.add("lock")
-//     })
-// })
+popupTextBtn.addEventListener('click', function (e) {
+    popupText.classList.add('open');
+    lockBobyScroll.classList.add('lock');
+})
+popupImageBtn.addEventListener('click', function (e) {
+    popupImage.classList.add('open');
+    lockBobyScroll.classList.add('lock');
+})
+popupTextCloseBtn.addEventListener('click', function (e) {
+    popupText.classList.remove('open');
+    lockBobyScroll.classList.remove('lock');
+})
+popupImageCloseBtn.addEventListener('click', function (e) {
+    popupImage.classList.remove('open');
+    lockBobyScroll.classList.remove('lock');
+})
+popupTextCloseBtnX.addEventListener('click', function (e) {
+    popupText.classList.remove('open');
+    lockBobyScroll.classList.remove('lock');
+})
+popupImageCloseBtnX.addEventListener('click', function (e) {
+    popupImage.classList.remove('open');
+    lockBobyScroll.classList.remove('lock');
+})
